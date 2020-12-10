4fe3a00533f1e348bfd7ef53455a12a65f3295f7
Dan Cassin <dancassin@gmail.com>
2020-12-10 08:45:00 -0800

    creating new function to get satellite image

--- satellite_image_utils.py
+++ satellite_image_utils.py
@@ -2,17 +2,23 @@ from dotenv import load_dotenv
 import os
 import requests
 import json
+import datetime
 
-def create_geographic_polygon():
+api_token = os.getenv('weather_api_key') #same api key as weather data
+san_diego_polygon = os.getenv('san_diego_polygon') #unique id for created polygon manually saved to .env
+
+current_datetime = datetime.now() # convert to UTC
+five_days_ago = datetime.now() - 0 #need to do the math for 3 days and convert to UTC
+
+def create_geographic_polygon(api_token):
     '''
     ****ONLY RUN THIS FCN ONCE: >2500ha RESULTS IN CHARGES*****
     - sends post request to argomonitoring.com to create polygon
     - only needs to be run once per polygon
-    - resulting JSON contains 
+    - resulting JSON contains identification
     '''
     load_dotenv()
 
-    api_token = os.getenv('weather_api_key') #same api key as weather data
     api_address = f'http://api.agromonitoring.com/agro/1.0/polygons?appid={api_token}'
 
     body = {
@@ -49,3 +55,8 @@ def create_geographic_polygon():
         json.dump(json_data, json_file)
 
     return json_data
+
+def get_satellite_image_from_API(api_token, polygon):
+    
+    api_address = f'http://api.agromonitoring.com/agro/1.0/image/search?start={start date}&end={end date}&polyid={polygon}&appid={api_token}'
+    pass
