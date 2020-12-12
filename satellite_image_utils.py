66831a26b8b083e94ab49717f8b214cd1357a739
Dan Cassin <dancassin@gmail.com>
2020-12-12 13:29:02 -0800

    building out satellite request, updating db

--- satellite_image_utils.py
+++ satellite_image_utils.py
@@ -3,12 +3,20 @@ import os
 import requests
 import json
 import datetime
+from datetime import timezone   
 
-api_token = os.getenv('weather_api_key') #same api key as weather data
+load_dotenv()
+
+satellite_api_token = os.getenv('weather_api_key') #same api key as weather data
+print(f'satellite_api_token: {satellite_api_token}')
 san_diego_polygon = os.getenv('san_diego_polygon') #unique id for created polygon manually saved to .env
+print(f'polygon: {san_diego_polygon}')
 
-current_datetime = datetime.now() # convert to UTC
-five_days_ago = datetime.now() - 0 #need to do the math for 3 days and convert to UTC
+current_datetime = datetime.datetime.now()
+current_UTC = int(current_datetime.replace(tzinfo=timezone.utc).timestamp())
+days = datetime.timedelta(5)
+five_days_ago = int((current_datetime - days).replace(tzinfo=timezone.utc).timestamp())
+
 
 def create_geographic_polygon(api_token):
     '''
@@ -56,7 +64,16 @@ def create_geographic_polygon(api_token):
 
     return json_data
 
-def get_satellite_image_from_API(api_token, polygon):
+
+def get_satellite_image_from_API(api_token, polygon, start_date, end_date):
     
-    api_address = f'http://api.agromonitoring.com/agro/1.0/image/search?start={start date}&end={end date}&polyid={polygon}&appid={api_token}'
-    pass
+    api_address = f'http://api.agromonitoring.com/agro/1.0/image/search?start={start_date}&end={end_date}&polyid={polygon}&appid={api_token}'
+
+    satellite_json = requests.post(api_address).json()
+    
+    return satellite_json
+
+
+result = get_satellite_image_from_API(satellite_api_token, san_diego_polygon, five_days_ago, current_UTC)
+
+print(result)
